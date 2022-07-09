73bebc0b844f023b054b86022931acee0231b434
Unreasonable motto <90086313+Athul1223@users.noreply.github.com>
2022-07-09 14:07:20 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/Athul1223/Deadpool.git /Deadpool
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /Deadpool
+fi
+cd /Deadpool
+pip3 install -U -r requirements.txt
+echo "Starting Deadpool...."
+python3 main.py
